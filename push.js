85090492233446d9b649350ad7580876cd0403f7
Vinkesh Kumar <vinkesh85@gmail.com>
2021-01-06 10:12:07 +0530

    push2

--- push.js
+++ push.js
@@ -13,7 +13,15 @@ let vapidKeys= {
 
   push.setVapidDetails('mailto:vinkesh85@gmail.com',vapidKeys.publicKey,vapidKeys.privateKey)
 
-  let sub={};
+  let sub={    
+  endpoint:
+  'https://fcm.googleapis.com/fcm/send/d9aMjOXnI4s:APA91bGtmaEnMc3mKFzVzN6xvatp-pQ-xFzHu_SZ8QeXnrqX6zLNseWoWJp3-AvbczRUt9g2pFqcW8C-hz9Ac4PhB9aTW23ydHTs3klY-wbTJW4zFn68aSfudButjx1XaQGqx59a0tHy',
+  expirationTime: null,
+  keys: {
+    p256dh:
+    'BASHea4bi2P0nmC-ynZV0mAGIB4Smh1EDmphzQBzTXZq1s_jKnz25Wo7TGEYEytr_naTa4B9-XsppeFCc_Xh3a8',
+    auth: 'A8fvK4paiG13RP5cdIBLcQ'
+  }}
 
   push.sendNotification(sub,'test message')
 
