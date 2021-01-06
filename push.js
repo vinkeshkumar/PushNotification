e6f07ffb0d2c2c972d5e184c01a20e9c6b5efae2
Vinkesh Kumar <vinkesh85@gmail.com>
2021-01-06 12:29:57 +0530

    changes

--- push.js
+++ push.js
@@ -1,27 +1,29 @@
-var push=require('web-push');
+var push = require('web-push');
 
 // this code is to generate vapids as we need to generate it once so we'll comment this and used geneated for future
 
-                        //let vapidKeys= push.generateVAPIDKeys();
-                        //console.log(vapidKeys);
+//let vapidKeys= push.generateVAPIDKeys();
+//console.log(vapidKeys);
 
-let vapidKeys= {
-    publicKey: 'BJfPHTQGHKhuTslWOjmFljv0HPjpW_vpZvQKsAbPA2fwU8IjWTVjprJx24ZkBNF2tcFu_fYqt49m3cqZa5Ffejc',
-    privateKey: 'D80XxdSxTy0myMLq1NKFqZKgXxz8JgdJDb_4NrkN_98'
-  }
+let vapidKeys = {
+  publicKey: 'BJfPHTQGHKhuTslWOjmFljv0HPjpW_vpZvQKsAbPA2fwU8IjWTVjprJx24ZkBNF2tcFu_fYqt49m3cqZa5Ffejc',
+  privateKey: 'D80XxdSxTy0myMLq1NKFqZKgXxz8JgdJDb_4NrkN_98'
+}
 
 
-  push.setVapidDetails('mailto:vinkesh85@gmail.com',vapidKeys.publicKey,vapidKeys.privateKey)
+push.setVapidDetails('mailto:vinkesh85@gmail.com', vapidKeys.publicKey, vapidKeys.privateKey)
 
-  let sub={    
+let sub = {
   endpoint:
-  'https://fcm.googleapis.com/fcm/send/d9aMjOXnI4s:APA91bGtmaEnMc3mKFzVzN6xvatp-pQ-xFzHu_SZ8QeXnrqX6zLNseWoWJp3-AvbczRUt9g2pFqcW8C-hz9Ac4PhB9aTW23ydHTs3klY-wbTJW4zFn68aSfudButjx1XaQGqx59a0tHy',
+    "https://fcm.googleapis.com/fcm/send/eSGCxyT6AXw:APA91bGGE3pWgSDjezM2jKHuY_HfebTIY7NQjoqhvQdRtfZyMRLBRwTYUdhBekPSdD6THg0eiMsEWRMO0CummInWCGWMmx0e3nhQU_PPsVLBW0UoP0zBtSWWaIT2rM3PO6gy8XCHz8zq",
   expirationTime: null,
-  keys: {
+  keys:
+  {
     p256dh:
-    'BASHea4bi2P0nmC-ynZV0mAGIB4Smh1EDmphzQBzTXZq1s_jKnz25Wo7TGEYEytr_naTa4B9-XsppeFCc_Xh3a8',
-    auth: 'A8fvK4paiG13RP5cdIBLcQ'
-  }}
+      "BCpYu5MgnfdbI3PHDE0t4ZW8CT4wYZcsMOSwMMiN409ftGv-w7N6V9vpnJpOp4gqMHVPLRa9NVsYSZAYfZhH53U",
+    auth: "67GVn2azwkj8I_y7Z4Ijag"
+  }
+};
 
-  push.sendNotification(sub,'test message')
+push.sendNotification(sub, 'test message')
 
